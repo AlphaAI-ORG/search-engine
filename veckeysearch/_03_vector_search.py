87c55402d5304aab39a42046cb38e67b8ee321cb
Jungeun17 <kkhj826@naver.com>
2025-06-09 21:18:24 +0900

    update embedding function

--- veckeysearch/_03_vector_search.py
+++ veckeysearch/_03_vector_search.py
@@ -1,25 +1,27 @@
 from .state import State
 from .utils import connect_db
-
 from langchain_openai import OpenAIEmbeddings
-from dotenv import load_dotenv
 
-def _get_embedding(text : str) -> list[float]:
+def _get_embedding(text : str, api_key: str) -> list[float]:
 
-    load_dotenv(override = True)
-    embeddings_model = OpenAIEmbeddings(model="text-embedding-3-large")
+    embeddings_model = OpenAIEmbeddings(
+        model="text-embedding-3-large",
+        openai_api_key=api_key,
+        )
+    
     embedding = embeddings_model.embed_query(text)
 
     return embedding 
 
-def vector_search(state : State):
+def vector_search(state : State, config):
     user_query = state["user_query"]
     index_name = state["index_name"]
     search_num = state["search_num"]
 
     client = connect_db()
 
-    user_query_vector = _get_embedding(text = user_query)
+    api_key = config['metadata']['api_key']
+    user_query_vector = _get_embedding(text = user_query, api_key = api_key)
 
     vector_hits = client.search(
             index=index_name,
